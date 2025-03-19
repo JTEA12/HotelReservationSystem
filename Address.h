f1b4404696b411224dbe2d36d6c855cbe237fd83
bachhalh1 <bachhalh@mcmaster.ca>
2025-03-19 16:45:30 -0400

    Address.h

--- Address.h
+++ Address.h
@@ -1,5 +1,33 @@
-#pragma once
-class Address
-{
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Address {
+private:
+    string street, city, state, country;
+    int zipCode;
+
+public:
+    // Constructors
+    Address();
+    Address(string st, string c, string s, string co, int zip);
+
+    // Getters
+    string getStreet() const;
+    string getCity() const;
+    string getState() const;
+    string getCountry() const;
+    int getZipCode() const;
+
+    // Setters
+    void setAddress(string st, string c, string s, string co, int zip);
+
+    // Print Address
+    void printAddress() const;
 };
 
+#endif // ADDRESS_H
