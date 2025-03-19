bfc50ed8e59283713a7bc77f1448639d2b73e441
bachhalh1 <bachhalh@mcmaster.ca>
2025-03-19 16:47:27 -0400

    Guest.cpp

--- Guest.cpp
+++ Guest.cpp
@@ -1,1 +1,28 @@
 #include "Guest.h"
+
+Guest::Guest() : firstName("Unknown"), lastName("Unknown"), email("Unknown"), phone("Unknown"), address() {}
+
+Guest::Guest(string fName, string lName, string mail, string ph, Address addr)
+    : firstName(fName), lastName(lName), email(mail), phone(ph), address(addr) {}
+
+string Guest::getFirstName() const { return firstName; }
+string Guest::getLastName() const { return lastName; }
+string Guest::getEmail() const { return email; }
+string Guest::getPhone() const { return phone; }
+Address Guest::getAddress() const { return address; }
+
+void Guest::setGuest(string fName, string lName, string mail, string ph, Address addr) {
+    firstName = fName;
+    lastName = lName;
+    email = mail;
+    phone = ph;
+    address = addr;
+}
+
+void Guest::printGuestInfo() const {
+    cout << "Guest Name: " << firstName << " " << lastName << endl;
+    cout << "Email: " << email << endl;
+    cout << "Phone: " << phone << endl;
+    cout << "Address: ";
+    address.printAddress();
+}
